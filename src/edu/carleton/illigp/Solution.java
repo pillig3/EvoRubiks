ae8c73bfd64620dd4da5ba20e695a525c8d27058
makalabay <makala.bay@gmail.com>
2017-05-25 03:43:00 -0500

    added description/comments to Solution.java

--- src/edu/carleton/illigp/Solution.java
+++ src/edu/carleton/illigp/Solution.java
@@ -1,18 +1,25 @@
 // package edu.carleton.illigp;
-
+ 
 /**
- * Created by peterillig on 5/8/17.
+ * This class represents a single proposed solution for a Rubik's cube as an int[] of 
+ * moves. It contains various constructors to create a solution from a given int[] or 
+ * to generate a random one. Methods are available to return the solution and its fitness, 
+ * perform crossover and mutation on the solution, and to duplicate the solution.
+ * 
+ * @author Peter Illig & Makala Hieshima
+ * @version 0.0.2
  */
  
 import java.util.*;
 
 public class Solution {
-
+	/** INITIAL VARIABLES **/
     private int[] genome;
     private int numPossibleInts;
     private double mutationProb; //between 0 and 1
     private double crossoverProb; //between 0 and 1
 
+	/** CONSTRUCTORS **/
     public Solution(int[] genome, int numPossibleInts, double mutationProb, double crossoverProb){
         this.genome = genome;
         this.numPossibleInts = numPossibleInts;
@@ -20,10 +27,7 @@ public class Solution {
         this.crossoverProb = crossoverProb;
     }
 
-    /*
-     * Initializes random genome
-     */
-    public Solution(int size, int numPossibleInts, double mutationProb, double crossoverProb){
+    public Solution(int size, int numPossibleInts, double mutationProb, double crossoverProb){ // initializes random genome
         genome = new int[size];
         for (int i = 0; i < size; i++) {
             genome[i] = (int)(Math.random()*numPossibleInts);
@@ -32,14 +36,19 @@ public class Solution {
         this.mutationProb = mutationProb;
         this.crossoverProb = crossoverProb;
     }
-
+	
     public Solution(){
         genome = new int[0];
         this.numPossibleInts = 0;
         this.mutationProb = 0;
         this.crossoverProb = 0;
     }
-
+	
+	/** BASIC METHODS **/
+	public int[] getGenome(){
+        return genome;
+    }
+    
 //     public double getFitness(int fitParam) { // QUESTION (MAK): Where does fitParam get used?
 //         int sum = 0;
 //         for (int i : genome) {
@@ -62,7 +71,8 @@ public class Solution {
 		rawFitness = rawFitness /100;
 		return rawFitness;
     }
-
+    
+    /** EA METHODS **/
     public void crossover(Solution other){
         int temp = 0;
         for (int i = 0; i < genome.length; i++) {
@@ -92,10 +102,7 @@ public class Solution {
         return new Solution(newGenome, numPossibleInts, mutationProb, crossoverProb);
     }
 
-    public int[] getGenome(){
-        return genome;
-    }
-
+	/** HELPER METHODS **/
     public String toString(){
         String output = "[";
         for (int i : genome) {
